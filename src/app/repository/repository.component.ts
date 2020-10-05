68ea5f9e121ef89b8d9b2ca374c0a92bdf1830ef
Bethwelkip <bkiplimo@princeton.edu>
2020-10-05 18:01:12 -0400

    add global variable

--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Repository } from '../repository'
 @Component({
   selector: 'app-repository',
   templateUrl: './repository.component.html',
   styleUrls: ['./repository.component.css']
 })
 export class RepositoryComponent implements OnInit {
+  repo: Repository;
+  searchRepository(repository) {
+    this.repo = new Repository();
+    this.repo.searchRepository(repository);
+
+  }
+  searchUsername(user) {
+
+    this.repo = new Repository();
+    this.repo.searchRepository(user);
+
+  }
 
   constructor() { }
 
