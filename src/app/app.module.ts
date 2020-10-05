9868711ca7d7eb3cf2ee9dc00d5d9fee57949afb
Bethwelkip <bkiplimo@princeton.edu>
2020-10-05 18:07:53 -0400

    add components and decorations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { MyrepoComponent } from './myrepo/myrepo.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    MyrepoComponent,
+    RepositoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
