3180fa4e1dd2172fbe21a4c1442fcb4e3e180093
Bethwelkip <bkiplimo@princeton.edu>
2020-10-05 18:05:06 -0400

    add functions to retrieve GitHub components

--- src/app/myrepo/myrepo.component.ts
+++ src/app/myrepo/myrepo.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ServService } from '../services/serv.service'
+import { User } from '../user'
 @Component({
   selector: 'app-myrepo',
   templateUrl: './myrepo.component.html',
   styleUrls: ['./myrepo.component.css']
 })
 export class MyrepoComponent implements OnInit {
+  mainUser: User;
+  userRepos: any;
+  constructor(private serv: ServService) {
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
+    this.serv.getUsers();
+    this.mainUser = this.serv.user;
+    this.serv.getRepos()
+    this.userRepos = this.serv.repository;
+    console.log(this.userRepos)
+
   }
 
 }
